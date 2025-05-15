6ac17c920a67ccb3b70fc9157377dd4a15d37e80
alekseylavrishin <78001914+alekseylavrishin@users.noreply.github.com>
2025-05-15 15:44:31 -0400

    Initial commit

---
+++ server/src/main/java/com/aleksey/yappr/YapprApplication.java
@@ -0,0 +1,13 @@
+package com.aleksey.yappr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YapprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YapprApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/aleksey/yappr/YapprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleksey.yappr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YapprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
